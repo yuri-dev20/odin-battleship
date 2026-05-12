6fcb1927365fb48873f1826ea974addc367d8758
yuri-dev20 <yuriglauco945@gmail.com>
2026-05-12 17:15:22 -0300

    improved dinamically creating UI

--- src/scripts/components/board.js
+++ src/scripts/components/board.js
@@ -2,15 +2,28 @@ export function createBoard() {
     const flexContainer = document.querySelector('#flex-container');
 
     const boardDiv = document.createElement('div');
-    const playerGrid = document.createElement('div');
-    const computerGrid = document.createElement('div');
+    const playerGrid = createGrid();
+    const computerGrid = createGrid();
 
-    playerGrid.textContent = "PLAYER GRID";
-    computerGrid.textContent = "COMPUTER GRID";
+    playerGrid.classList.add("playersGrid")
+    computerGrid.classList.add("playersGrid");
     
     boardDiv.classList.add('gameBoard');
     boardDiv.appendChild(playerGrid);
     boardDiv.appendChild(computerGrid);
 
     flexContainer.appendChild(boardDiv);
+}
+
+function createGrid() {
+    const grid = document.createElement("div");
+    grid.classList.add("playersGrid");
+
+    for (let i = 0; i < 100; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("boardCell");
+        grid.appendChild(cell);
+    }
+
+    return grid;
 }
