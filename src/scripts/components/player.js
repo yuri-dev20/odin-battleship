2156ec1736139df02e60be1abe0caaf2f6a98678
yuri-dev20 <yuriglauco945@gmail.com>
2026-05-14 15:38:22 -0300

    implement Player class with tests

--- src/scripts/components/player.js
+++ src/scripts/components/player.js
@@ -1,5 +1,9 @@
 import { GameBoard } from "./gameBoard.js";
 
 export class Player {
-    
+    constructor(type) {
+        this.type = type;
+        this.board = new GameBoard();
+        this.board.placeShips();
+    }
 }
--- src/scripts/components/player.test.js
+++ src/scripts/components/player.test.js
@@ -5,4 +5,23 @@ describe('Player', () => {
         const player = new Player('human');
         expect(player.type).toBe('human');
     });
+
+    test('should create a computer player', () => {
+        const computer = new Player('computer');
+        expect(computer.type).toBe('computer');
+    });
+
+    test('should create a game board for player', () => {
+        const player = new Player('human');
+        expect(player.board).toBeDefined();
+    });
+
+    test('should place ships automatically', () => {
+        const player = new Player('human');
+        const hasShips = player.board.grid.some(row => 
+            row.some(cell => cell !== null)
+        );
+
+        expect(hasShips).toBe(true);
+    });
 });
